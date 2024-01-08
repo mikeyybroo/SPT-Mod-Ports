0be8e427a1f6a3168f31743d406aa28428d474c7
Bob S <shibdib@users.noreply.github.com>
2024-01-08 00:27:39 -0500

    Revert "Filter quests with no accessible objectives due to SIT pathing weirdness"
    This reverts commit 4da4be7d896a1dd6bc2047a27df9234179ef8a9a.

--- SPTQuestingBots-SIT/Controllers/Bots/BotJobAssignmentFactory.cs
+++ SPTQuestingBots-SIT/Controllers/Bots/BotJobAssignmentFactory.cs
@@ -11,7 +11,6 @@ using SPTQuestingBots.BotLogic.Objective;
 using SPTQuestingBots.Models;
 using Comfort.Common;
 using UnityEngine;
-using UnityEngine.AI;
 
 namespace SPTQuestingBots.Controllers.Bots
 {
@@ -523,35 +522,11 @@ namespace SPTQuestingBots.Controllers.Bots
 
         public static QuestQB GetRandomQuest(this BotOwner bot, IEnumerable<QuestQB> invalidQuests)
         {
-            NavMeshPath Path = new NavMeshPath();
             // Group all valid quests by their priority number in ascending order
             var groupedQuests = allQuests
                 .Where(q => !invalidQuests.Contains(q))
                 .Where(q => q.NumberOfValidObjectives > 0)
                 .Where(q => q.CanMoreBotsDoQuest())
-                .Where(q =>
-                {
-                    bool validQuest = false;
-                    foreach (var objective in q.ValidObjectives)
-                    {
-                        var objectivePosition = objective.GetFirstStepPosition();
-                        if (objectivePosition.HasValue)
-                        {
-                            Vector3 position = objectivePosition.Value;
-                            NavMesh.CalculatePath(bot.Position, (Vector3)position, -1, Path);
-                            if (Path.status == NavMeshPathStatus.PathComplete)
-                            {
-                                validQuest = true;
-                            }
-                            q.TryRemoveObjective(objective);
-                        }
-                    }
-                    if (validQuest == false)
-                    {
-                        allQuests.Remove(q);
-                    }
-                    return validQuest;
-                })
                 // .Where(q => q.CanAssignToBot(bot)) // TODO: Fixme -> Workaround, if there are no quests available for bots, it may start throwing exceptions like no tomorrow.
                 .GroupBy
                 (
